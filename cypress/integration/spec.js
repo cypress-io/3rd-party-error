d3db762d76aeeb5c162fd29935115c9861179dd2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-08-15 09:49:47 -0400

    initial failing test

---
+++ cypress/integration/spec.js
@@ -0,0 +1,7 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="Cypress" />
+
+it('loads guardian uk', () => {
+  cy.visit('https://www.theguardian.com/uk')
+})
